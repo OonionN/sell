c58db2dc2b1a596ceb6a73a6e71484635d62390c
fei PENG <2577718416@qq.com>
2021-10-10 18:57:49 +0800

    init

---
+++ src/test/java/com/peng/LogTest.java
@@ -0,0 +1,25 @@
+package com.peng;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LogTest {
+
+    @Test
+    public void test1(){
+        String name = "peng";
+        String passwd = "123456";
+        log.debug("debug...");
+        log.info("info...");
+        log.info("name: {}, passwd: {}", name, passwd);
+        log.error("error...");
+    }
+}
